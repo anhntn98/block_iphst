5bbab8a0088a9fd678643629304038f0b2c63f99
anhntn98 <121283594+anhntn98@users.noreply.github.com>
2024-07-25 15:20:42 +0700

    Update navigation.py

--- block_iphst/navigation.py
+++ block_iphst/navigation.py
@@ -2,7 +2,7 @@ from extras.plugins import PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
 
 bl_items = PluginMenuItem(
-        link='plugins:block_iphst:blockiphst',
+        link='plugins:block_iphst:BlockIp',
         link_text='Block IP HST',
         staff_only=True
     )
