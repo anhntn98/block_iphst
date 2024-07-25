630988db11794266b772c9238dacaa389156d1c2
anhntn98 <121283594+anhntn98@users.noreply.github.com>
2024-07-25 15:26:15 +0700

    Update __init__.py

--- block_iphst/__init__.py
+++ block_iphst/__init__.py
@@ -5,7 +5,7 @@ class BlockIpHstConfig(PluginConfig):
     name = 'block_iphst'
     verbose_name = ' Block IP zone HST'
     description = ''
-    version = '0.2.5'
+    version = '0.2.6'
 
 
 config = BlockIpHstConfig
