d1ebf97975e4ef91cd388b6cb445a20c2f9ee5e5
atom-tr <thai.tr@hotmail.com>
2024-07-25 15:08:38 +0700

    edit nagi

--- block_iphst/navigation.py
+++ block_iphst/navigation.py
@@ -1,8 +1,9 @@
 from extras.plugins import PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
 
-menu_items = PluginMenuItem(
+bl_items = PluginMenuItem(
         link='plugins:block_iphst:blockiphst',
         link_text='Block IP HST',
         staff_only=True
     )
+menu_items=[bl_items]
