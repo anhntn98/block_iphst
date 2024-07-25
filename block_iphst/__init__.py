1065850aa3b6ac092b95a9f8d3f2f4a49c97e263
atom-tr <thai.tr@hotmail.com>
2024-07-25 15:40:57 +0700

    update

--- block_iphst/__init__.py
+++ block_iphst/__init__.py
@@ -5,7 +5,7 @@ class BlockIpHstConfig(PluginConfig):
     name = 'block_iphst'
     verbose_name = ' Block IP zone HST'
     description = ''
-    version = '0.2.6'
+    version = '0.2.7'
 
 
 config = BlockIpHstConfig
