c8396c334e62fad3023c83557bba364abf3992a8
anhntn98 <121283594+anhntn98@users.noreply.github.com>
2024-07-25 15:26:33 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='block_iphst',
-    version='0.2.5',
+    version='0.2.6',
     description='Block IP zone hosting',
     install_requires=[],
     packages=find_packages(),
