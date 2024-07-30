0539a7ea672ac36f4d1f64e9af450d69ce7ec818
anhntn98 <121283594+anhntn98@users.noreply.github.com>
2024-07-30 09:46:38 +0700

    Update urls.py

--- block_iphst/urls.py
+++ block_iphst/urls.py
@@ -5,6 +5,8 @@ from . import  views
 
 
 urlpatterns = [
+    path('add/'.views.add.as_view(),name='add'),
+    path('remove/'.views.add.as_view(),name='remove')
     path('', views.BlockIP.as_view(), name='BlockIp'),
 ]
 
