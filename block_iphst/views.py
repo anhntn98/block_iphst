224e03a4279c6762b6365fb75adb5f9f6bffbf23
anhntn98 <121283594+anhntn98@users.noreply.github.com>
2024-07-31 14:35:36 +0700

    Update views.py

--- block_iphst/views.py
+++ block_iphst/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views import View
 from ipam.models import Prefix
 import ipaddress
+import .configuration
+# Juniper
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from jnpr.junos.exception import ConnectError, ConfigLoadError, CommitError, ConnectAuthError, RpcTimeoutError
 
 def check_ip_in_prefix(ip):
     try:
@@ -13,7 +18,10 @@ def check_ip_in_prefix(ip):
     ipaddress.ip_network('202.151.168.0/24'),
     ipaddress.ip_network('210.86.239.0/24')
     ]
-    return any(ip in network for network in networks)
+    for network in networks:
+        if ip in network:
+            return network
+    return False
 
 class BlockIP(View):
     template_name = 'block_iphst/blockip.html'
@@ -32,8 +40,14 @@ class Add_Block(View):
     )
     def post(self,request):
         ip=request.POST.get("IP","")
-        if check_ip_in_prefix(ip):
-            
+        prefix=check_ip_in_prefix(ip)
+        if prefix:
+            for de in gw:
+                with Device(de) as dev:
+                    with Config(dev, mode='private') as conf_dev:
+                        commit_cmd = f'set interfaces ae10 unit 0 family inet address {prefix} arp {ip} mac 00:01:00:02:00:03'
+                        
+                        
             
             
             
