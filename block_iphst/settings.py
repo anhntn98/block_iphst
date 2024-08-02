7497c2bb70eb7c26ba6ccb99ea0a45304f16e08e
anhntn98 <121283594+anhntn98@users.noreply.github.com>
2024-08-02 09:46:10 +0700

    Update settings.py

--- block_iphst/settings.py
+++ block_iphst/settings.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-gw=[
-  {host="1.1.1.1",user="username", passwd="password"},
-  {host="2.2.2.2",user="username", passwd="password"}
-]
+gw=[ {"host":"1.1.1.1","user":"username", "passwd":"password"},  
+    {"host":"2.2.2.2","user":"username", "passwd":"password"}
+  ]
