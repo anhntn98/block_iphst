919983095e249d7c0f7624071d3568dac2de3e98
anhntn98 <121283594+anhntn98@users.noreply.github.com>
2024-08-02 11:21:46 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='block_iphst',
-    version='0.3.11',
+    version='0.3.12',
     description='Block IP zone hosting',
     install_requires=['junos-eznc'],
     packages=find_packages(),
