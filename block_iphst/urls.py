609865501ac3b48cfa94b9cb30f94bd9218c0041
anhntn98 <121283594+anhntn98@users.noreply.github.com>
2024-08-02 11:06:19 +0700

    Update urls.py

--- block_iphst/urls.py
+++ block_iphst/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from netbox.views.generic import ObjectChangeLogView
 from . import  views
 
-
+app_name = 'block_iphst'
 urlpatterns = [
     path('add/', views.Add_Block.as_view(),name='add'),
     path('remove/', views.Remove_Block.as_view(),name='remove'),
